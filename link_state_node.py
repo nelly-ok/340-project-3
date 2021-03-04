c47c1de005898703fafd0957a608eb4bc459faac
nelly-ok <okunlolanelson@gmail.com>
2021-03-03 18:11:02 -0600

    update

--- link_state_node.py
+++ link_state_node.py
@@ -13,7 +13,7 @@ class Link_State_Node(Node):
         self.weights = {} #weights keyed by edges
         self.seq = {}
         self.lastMsgs ={}
-        self.nodes = [] #lof nodes to help with dijsktras
+        self.nodes = [id] #lof nodes to help with dijsktras
 
     # Return a string
     #can be mostly ignored, used for debugging
@@ -29,29 +29,31 @@ class Link_State_Node(Node):
     # if a new link is added, update your graph, then create a message to update your neighbors
     def link_has_been_updated(self, neighbor, latency):
         edge = frozenset({self.id, neighbor})
+        time = self.get_time()
+        self.seq[edge] = time
         if latency == -1 and edge in self.edges: #remove the edge if you have to
             self.edges.remove(edge)
             del self.weights[edge]
             del self.seq[edge]
             del self.lastMsgs[edge]
-            for node in edge:
-                self.nodes.remove(node)
-            return
-        if edge not in self.edges: #else if its not already in the graph, add it
-            self.edges.append(edge)
-            for node in edge:
-                self.nodes.append(node)
-        self.weights[edge] = latency #update the cost if necessary
-        if edge in self.seq: self.seq[edge] +=1 #update the sequence number or initialize it
-        else: self.seq[edge] = 0
-
-
+            self.nodes.remove(neighbor)
+        else: 
+            if edge not in self.edges: #else if its not already in the graph, add it
+                self.edges.append(edge)
+                self.nodes.append(neighbor)
+                for key, value in self.lastMsgs.items():
+                    self.send_to_neighbor(neighbor, value)
+            
+            self.weights[edge] = latency #update the cost if necessary
+            # if edge in self.seq: self.seq[edge] +=1 #update the sequence number or initialize it
+            # else: self.seq[edge] = 0
         #retransmit all your most recent messages from each node - do this because a new node doesn't have information about the graphh so it needs it
         #also send along this new message 
-        message = json.dumps({"src": self.id, "dest": neighbor, "cost": latency, "seq": self.seq[edge]}) #send message
+        message = json.dumps({"src": self.id, "dest": neighbor, "cost": latency, "seq": time}) #send message
+        self.send_to_neighbors(message)
         self.lastMsgs[edge] = message
-        for key, value in self.lastMsgs.items():
-            self.send_to_neighbors(value)
+        # for key, value sin self.lastMsgs.items():
+        #     self.send_to_neighbors(value)
         # latency = -1 if delete a link
 
 
@@ -60,25 +62,38 @@ class Link_State_Node(Node):
     #if you get an old message, get your neighbors up to date
     #else either add this new connection, or update, and retransmit to your neighbors
     def process_incoming_routing_message(self, m):
-       
-
-
         m = json.loads(m)
         edge = frozenset({m["src"], m["dest"] })
         #if old message, update your neighbors with your latest information concerning this edge
-        if edge in self.edges and m["seq"] < self.seq[edge]:
-            self.send_to_neighbors(self.lastMsgs[edge])
-        else: 
-            if edge not in self.edges: #if youu don't have this edge add it
-                self.edges.append(edge)
-                self.nodes.append(m["src"])
-                self.nodes.append(m["dest"])
-            #update accordingly
-            self.weights[edge] = m["cost"]
+        
+        if edge not in self.seq or m["seq"] > self.seq[edge]:
             self.seq[edge] = m["seq"]
-            self.lastMsgs[edge] = json.dumps(m)
-            for key, value in self.lastMsgs.items(): #retransmit this message (along with your most reccent messages incase you hade to inform a newly added nodee)
-                self.send_to_neighbors(value)
+            if m["cost"] == -1 and edge in self.edges: #remove the edge if you have to
+                self.edges.remove(edge)
+                del self.weights[edge]
+                #del self.seq[edge]
+                del self.lastMsgs[edge]
+                for node in edge:
+                    if node != id:
+                        self.nodes.remove(node)
+            else: 
+                if edge not in self.edges: #if youu don't have this edge add it
+                    self.edges.append(edge)
+                    if m["src"] != id:
+                        self.nodes.append(m["src"])
+                    if m["dest"] != id:
+                        self.nodes.append(m["dest"])
+                    for key, value in self.lastMsgs.items():
+                        self.send_to_neighbor(m["src"], value)
+                        self.send_to_neighbor(m["dest"], value)
+                #update accordingly
+                self.weights[edge] = m["cost"]
+                self.lastMsgs[edge] = json.dumps(m)
+            #eeverrything not source
+            self.send_to_neighbors(json.dumps(m)) #retransmit the message
+        else: 
+            if m["seq"] < self.seq[edge]:
+                self.send_to_neighbor(m["src"], self.lastMsgs[edge])
 
 
      
